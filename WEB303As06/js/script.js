5e2240783f66e2c9c7b349235bd4ed8d21ba6d4c
HarshdeepHarshdeep <W0788912@myscc.ca>
2022-11-04 23:04:03 -0400

    Assignment 6

---
+++ WEB303As06/js/script.js
@@ -0,0 +1,27 @@
+/*
+    Student Name: Harshdeep Harshdeep
+    ID: 0788912
+    Date: 03-10-22
+   Assignment 6:
+*/
+$(document).ready(function(){
+  //Accordion 
+  $('.accordion_tab').on('click',function(){
+      let $accordion=$(this).closest('.accordion_wrapper');
+      $accordion.find('.accordion_tab.active').removeClass("active");
+      $(this).toggleClass('active');
+     
+  });
+  //Tabs
+  $('.tabs li').on('click',function(){
+      let panel=$(this).closest('.tab-panels');
+      panel.find('.tabs li.active').removeClass('active');
+      $(this).addClass('active');
+       //figure out which panel to show
+       var panelToShow=$(this).attr('rel');
+       panel.find('.panel.active').removeClass('active');
+       $('#'+panelToShow).addClass('active');
+    
+  
+  });
+  });
